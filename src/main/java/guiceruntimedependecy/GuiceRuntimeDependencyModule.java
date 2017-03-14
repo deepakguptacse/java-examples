3c55fdbda891d2e0f9dd4a0e8036f0805ff6d9ff
Deepak Gupta <gudee@amazon.com>
2017-03-14 16:54:17 +0000

    [guiceruntimedependency] Change guice dao binding to singleton.

--- src/main/java/guiceruntimedependecy/GuiceRuntimeDependencyModule.java
+++ src/main/java/guiceruntimedependecy/GuiceRuntimeDependencyModule.java
@@ -3,6 +3,7 @@ package guiceruntimedependecy;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.Scopes;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 
@@ -11,7 +12,7 @@ import java.util.Scanner;
 public class GuiceRuntimeDependencyModule extends AbstractModule {
 
     protected void configure() {
-        bind(ExampleDao.class).annotatedWith(Names.named("sql")).to(SqlDao.class);
-        bind(ExampleDao.class).annotatedWith(Names.named("nosql")).to(NosqlDao.class);
+        bind(ExampleDao.class).annotatedWith(Names.named("sql")).to(SqlDao.class).in(Scopes.SINGLETON);
+        bind(ExampleDao.class).annotatedWith(Names.named("nosql")).to(NosqlDao.class).in(Scopes.SINGLETON);
     }
 }
